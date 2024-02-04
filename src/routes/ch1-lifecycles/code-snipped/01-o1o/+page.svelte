c88eb2b0e61868f0c7fd2c7545243ab86981fabc
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-04 20:43:43 +0100

    01-o1o

---
+++ src/routes/ch1-lifecycles/code-snipped/01-o1o/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount, beforeUpdate, afterUpdate, onDestroy } from "svelte";
+  
+  let count = 0;
+  
+  onMount(() => {
+    console.log("onMount!");
+  });
+  
+  beforeUpdate(() => {
+    console.log("beforeUpdate!");
+  });
+  
+  afterUpdate(() => {
+    console.log("afterUpdate!");
+  });
+  
+  onDestroy(() => {
+    console.log("onDestroy!");
+  });
+</script>
+
+<button
+  on:click={() => {
+    count++;
+  }}
+>
+  Counter: {count}
+</button>
