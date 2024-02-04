c6b04566cf32aa598749e35cee86ee1b4dfef053
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-04 20:44:51 +0100

    04-break-init-lifecycle

---
+++ src/routes/ch1-lifecycles/code-snipped/04-break-init-lifecycle/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { onMount } from "svelte";
+  function buttonClicked() {
+    onMount(() => console.log("onMount!"));
+  }
+</script>
+
+<button on:click={buttonClicked}>Get error for free 🥳</button>
+
+<p>Look at your browser console.</p>
