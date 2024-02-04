6270402fadb6aa8a272a344d1c6ffbe4e53535a6
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-04 20:44:20 +0100

    02-bindings-demo + 03-mounting-phase

---
+++ src/routes/ch1-lifecycles/code-snipped/02-bindings-demo/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { afterUpdate, beforeUpdate, onMount } from "svelte";
+
+  let element: HTMLElement | undefined;
+
+  console.log("🚀 ~ 5 ~ element:", element);
+
+  beforeUpdate(() => {
+    console.log("🚀 ~ 9 ~ element:", element);
+  });
+
+  onMount(() => {
+    console.log("🚀 ~ 13 ~ element:", element);
+  });
+
+  afterUpdate(() => {
+    console.log("🚀 ~ 17 ~ element:", element);
+  });
+
+  console.log("🚀 ~ 20 ~ element:", element);
+</script>
+
+<div bind:this={element}>This is a div element.</div>
---
+++ src/routes/ch1-lifecycles/code-snipped/03-mounting-phase/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  function action(node: HTMLElement) {
+    // wat i can do here
+    node.setAttribute("data-custom-attribute", "some value");
+
+    const newElement = document.createElement("p");
+    newElement.textContent = "New element";
+    node.appendChild(newElement);
+  }
+</script>
+
+<div use:action />
