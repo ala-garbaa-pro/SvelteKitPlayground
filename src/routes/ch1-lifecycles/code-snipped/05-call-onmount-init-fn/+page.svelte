9c41482a22cd76c9b3bdd886cd5b24f065aaa25a
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-04 20:45:00 +0100

    05-call-onmount-init-fn

---
+++ src/routes/ch1-lifecycles/code-snipped/05-call-onmount-init-fn/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { onMount } from "svelte";
+  setup();
+  function setup() {
+    onMount(() => console.log("onMount! wahh 😄 the component is still initializing, this is perfectly fine"));
+  }
+</script>
+
+<p>Look at your browser console.</p>
