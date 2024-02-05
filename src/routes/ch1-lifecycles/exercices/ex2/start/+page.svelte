190d35a6fd37b96c838768161a7a67b89f45c3b5
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-05 20:26:17 +0100

    start

---
+++ src/routes/ch1-lifecycles/exercices/ex2/start/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Fiesta from "../ui/Fiesta.svelte";
+  import Palooza from "../ui/Palooza.svelte";
+  import Wham from "../ui/Wham.svelte";
+  import clown from "./clown.png";
+</script>
+
+<Palooza />
+
+<Fiesta />
+
+<Wham />
+
+<div class="bg-[#baac87] flex items-center flex-col">
+  <img style="width:30vw;" alt="clown" src={clown} />
+
+  <div class="h-[500vh] w-full bg-green-900" />
+</div>
