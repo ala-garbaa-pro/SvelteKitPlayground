9bf1e32612c4ce735d18d9bc9ec7d27bed8d39f2
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-07 21:40:58 +0100

    Update +page.svelte

--- src/routes/l3-props-and-state/code-snippet/js-ref-by-value-or-obj-primitive/+page.svelte
+++ src/routes/l3-props-and-state/code-snippet/js-ref-by-value-or-obj-primitive/+page.svelte
@@ -9,5 +9,5 @@ Check your console
 Check your console When passing an object as a prop, it's important to
 understand that objects in JavaScript are passed by reference. This means that
 when you pass an object from one component to another as a prop, both components
-are referencing the same object in memory. Therefore, changes made to the object
+are referencing the same object in . Therefore, changes made to the object
 in one component will be reflected in the other component as well.
