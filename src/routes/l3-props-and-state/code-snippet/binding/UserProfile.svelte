47a2b1100f70cfad16ca6d5a9bae9ef72b4d933a
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-13 21:23:54 +0100

    Update UserProfile.svelte

--- src/routes/l3-props-and-state/code-snippet/binding/UserProfile.svelte
+++ src/routes/l3-props-and-state/code-snippet/binding/UserProfile.svelte
@@ -6,6 +6,12 @@
 
 Username: <br />
 <input
+  on:input={(e) => {
+    username = e.target.value;
+  }}
+
+  on:focus={() => console.log("focus")}
+  on:blur={() => console.log("blur")}
 
 
 bind:value={username} />!
