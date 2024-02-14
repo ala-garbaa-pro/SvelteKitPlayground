c7366e5feaffcc4dd69d47e997c5d1ffd1cae89c
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-14 20:33:52 +0100

    add DateLabel

---
+++ src/routes/l3-props-and-state/code-snippet/Deriving states from props with a reactive declaration/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+
+  import DateLabel from "./DateLabel.svelte";
+
+</script>
+<DateLabel date={new Date(2024,5,5)} />
---
+++ src/routes/l3-props-and-state/code-snippet/Deriving states from props with a reactive declaration/DateLabel.svelte
@@ -0,0 +1,10 @@
+<!-- filename: DateLabel.svelte -->
+<script lang="ts">
+  export let date: Date;
+  export let locale: string;
+  // Reactive declaration with multiple dependencies
+  $: label = date.toLocaleDateString(locale);
+</script>
+
+<label for="date">{label}</label>
+<input class="w-full" type="text" bind:value={date} />
