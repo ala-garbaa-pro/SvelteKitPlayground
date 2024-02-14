24e920ad39df0db4c86de5661ed50fcfa54e104c
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-14 21:34:35 +0100

    Update CrazyMath.svelte

--- src/routes/l3-props-and-state/code-snippet/Deriving states from props with a reactive declaration/CrazyMath.svelte
+++ src/routes/l3-props-and-state/code-snippet/Deriving states from props with a reactive declaration/CrazyMath.svelte
@@ -5,10 +5,11 @@
   $: valueC = valueA + 5;
 
   $: valueD = valueB + valueC;
-  
+
   $: valueE = Math.min(valueC, valueD / 2);
 </script>
 
 <h1>{valueA} + {valueB} = {valueC}</h1>
+
 <h1>{valueC} + {valueD} = {valueD}</h1>
 <h1>{valueD} / 2 = {valueE}</h1>
