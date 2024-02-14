8dc859cafe5b8d6e59c4493ef9c4e2620804f312
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-14 21:20:28 +0100

    up

---
+++ src/routes/l3-props-and-state/code-snippet/updating derived states/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Val23 from "./Val23.svelte";
+
+  let val = 1;
+</script>
+
+<Val23 bind:value={val} />
---
+++ src/routes/l3-props-and-state/code-snippet/updating derived states/Val23.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let value: number;
+  $: double = value * 2;
+  $: triple = value * 3;
+</script>
+
+<input bind:value={double} type="number" />
+<input bind:value={triple} type="number" />
