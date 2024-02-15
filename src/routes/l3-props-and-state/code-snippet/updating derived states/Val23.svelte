971c0b454c452f64430d5f223fd4ef8154cdca6c
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-15 20:33:49 +0100

    Update Val23.svelte

--- src/routes/l3-props-and-state/code-snippet/updating derived states/Val23.svelte
+++ src/routes/l3-props-and-state/code-snippet/updating derived states/Val23.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let value: number;
-  
+
   $: double = value * 2;
 
   $: triple = value * 3;
@@ -11,4 +11,5 @@
 </script>
 
 <input bind:value={double} type="number" />
+
 <input bind:value={triple} type="number" />
