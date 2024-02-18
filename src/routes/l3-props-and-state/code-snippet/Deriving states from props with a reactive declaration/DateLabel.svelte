29091e5a1c318bccdd7c0d841f87765e66bf893e
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-18 22:26:45 +0100

    Update DateLabel.svelte

--- src/routes/l3-props-and-state/code-snippet/Deriving states from props with a reactive declaration/DateLabel.svelte
+++ src/routes/l3-props-and-state/code-snippet/Deriving states from props with a reactive declaration/DateLabel.svelte
@@ -6,6 +6,8 @@ export let locale: string;
 // Reactive declaration with multiple 
 $: label = date.toLocaleTimeString(locale);
 </script>
+
 <label for="date">{label}</label>
+
 
 <input class="w-full" type="text" bind:value={date} />
