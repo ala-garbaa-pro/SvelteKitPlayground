e665b028392f0fd9724abe01cd17c846087f37dd
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-19 20:39:41 +0100

    Passing dynamic content through slots

--- src/routes/l4-composing-components/code-snippet/+page.svelte
+++ src/routes/l4-composing-components/code-snippet/+page.svelte
@@ -1,4 +1,8 @@
 <a href="/l4-composing-components/code-snippet/controlling-through-props">Controlling through props</a>
 <br />
+
 <a href="/l4-composing-components/code-snippet/controlling-through-context">Controlling through context</a>
+<br />
+
+<a href="/l4-composing-components/code-snippet/passing-dynamic-content-through-slots">Passing dynamic content through slots</a>
 <br />
---
+++ src/routes/l4-composing-components/code-snippet/passing-dynamic-content-through-slots/Card.svelte
@@ -0,0 +1,16 @@
+<div class="card">
+  <slot />
+</div>
+
+<style>
+  .card {
+    border: 1px solid black;
+    padding: 1rem;
+    margin: 1rem;
+    max-width: 20rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    background: white;
+    color: black;
+  }
+</style>
