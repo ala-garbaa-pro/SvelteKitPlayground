8d2cc37b81e1d98433fa84ac5fdcfe64629a8e50
Ala GARBAA <ala.garbaa.pro@gmail.com>
2024-02-19 20:37:10 +0100

    Controlling through context

---
+++ src/routes/l4-composing-components/code-snippet/controlling-through-context/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+  import Child from "./Child.svelte";
+
+  setContext("message", "Hello from parent");
+</script>
+
+<Child />
---
+++ src/routes/l4-composing-components/code-snippet/controlling-through-context/Child.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { getContext } from "svelte";
+  const message = getContext("message");
+</script>
+
+<p>{message}</p>
